9e9ebbc63352028616f27aaa224876d82993ae45
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-06 19:24:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0]; //breaking everything line-by-line to understand the DOM concepts
 
 image1.setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // fusing the variables together to understand how it can be organized into one line and not clutter
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); 
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
