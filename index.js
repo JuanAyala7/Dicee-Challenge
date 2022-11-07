665babd3e8dde242a07689a80c7200691b384445
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-06 19:25:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // results to choosing a random number  1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0]; //breaking everything line-by-line to understand the DOM concepts
+var image1 = document.querySelectorAll("img")[0]; // breaking everything line-by-line to understand the DOM concepts
 
 image1.setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // fusing the variables together to understand how it can be organized into one line and not clutter
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // fusing the variables together to understand what it means to not clutter as a developer.
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); 
 
